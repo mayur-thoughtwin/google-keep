e9e4756769d4d962ac253be37cf371c80467c8f2
mayur-thoughtwin <mayur.panchal@thoughtwin.com>
2025-08-05 18:52:08 +0530

    refactor/upload-image

---
+++ src/middlewares/pathfinder.middleware.ts
@@ -0,0 +1,9 @@
+import { FieldMiddleware, MiddlewareContext, NextFn } from '@nestjs/graphql';
+
+export const pathFinderMiddleware: FieldMiddleware = async (
+  ctx: MiddlewareContext,
+  next: NextFn,
+) => {
+  const filePath: string = await next();
+  return `http://localhost:3000/${filePath}`;
+};
---
+++ src/utils/reponse.ts
@@ -0,0 +1,16 @@
+export const handleResponse = ({
+  success,
+  message,
+  data,
+}: {
+  success: boolean;
+  message: string | Error;
+  data?: any;
+}) => {
+  return {
+    success,
+    message: typeof message === 'string' ? message : message.message,
+    timestamp: new Date().toISOString(),
+    ...(data !== undefined && data !== null && { data }),
+  };
+};
